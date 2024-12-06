af0770a2c3997ef5fbeae8e363b22e396a34f422
Kurt Jensen <98291180+Hockeykurt@users.noreply.github.com>
2024-12-06 16:19:48 -0500

    Time: 0 ms (100%), Space: 8.8 MB (15.08%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(char ch : s){
+            if (ch == '[' || ch == '{' || ch == '(') {
+                stk.push(ch);
+            }
+            else if (ch == ']' || ch == '}' || ch == ')'){
+                if (stk.empty()) {
+                    return false;  
+                }
+                char top = stk.top();
+                if ((ch == ']' && top == '[') || 
+                (ch == '}' && top == '{') || 
+                (ch == ')' && top == '(')) {
+                    stk.pop();  
+                } 
+                else {
+                    return false; 
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
